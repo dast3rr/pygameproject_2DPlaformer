2cdd2f8cec71c964d32cb8b395f27452b633e94e
The_Mixa <misha.oleg548@gmail.com>
2024-01-19 01:57:25 +0500

    концовка. логика стен

--- code/triggers.py
+++ code/triggers.py
@@ -1,6 +1,6 @@
 from graphics import Platform, platforms, vertical_platforms, trigger_blocks, character, screen, load_image, enemies, \
-    Vengefly, main_character, background_image
-from data import global_cords, N, respawn_cords
+    Vengefly, main_character, background_image, N
+from data import global_cords, respawn_cords
 import pygame
 from load_music import battle_music, first_loc_music
 
@@ -13,12 +13,18 @@ class Boss_Wall_Lock(pygame.sprite.Sprite):
         self.image.set_alpha(0)
         self.lock_wall = False
         self.boss = None
+        self.block_wall_exist = False
+
 
     def update(self):
+        if not self.block_wall_exist:
+            self.block_wall = Platform(1050, 1200, 20, 100, platforms, vertical_platforms)
+            platforms.draw(screen)
+            self.block_wall_exist = True
         if pygame.sprite.spritecollideany(self, character) and not self.lock_wall:
             self.lock_wall = True
             x, y, a, b = -40, -100, 10, 200
-            Platform(x, y, a, b, platforms, vertical_platforms)
+            self.block_wall_boss = Platform(x, y, a, b, platforms, vertical_platforms)
             platforms.draw(screen)
 
             mouthwing_graphics = []
@@ -46,27 +52,27 @@ class Boss_Wall_Lock(pygame.sprite.Sprite):
         if self.boss is not None:
             if self.boss.hp == 0:
                 self.boss.hp = 1
-                MindSphere(self.boss.rect.x, self.boss.rect.y - 200)
                 first_loc_music()
                 pygame.mixer.music.play(-1, fade_ms=50)
 
+                trigger_blocks.remove(self)
+                self.block_wall.kill()
+                self.block_wall_boss.kill()
+                platforms.draw(screen)
 
-class MindSphere(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+
+class WhiteLight(pygame.sprite.Sprite):
+    def __init__(self):
         super().__init__(trigger_blocks)
-        self.rect = pygame.rect.Rect(x, y, 50, 50)
-        self.image = load_image('effects\\white_sphere.png')
-        self.image = pygame.transform.scale(self.image, (500, 500))
-        self.mask = pygame.mask.from_surface(self.image)
+        self.image = load_image('effects\\white_light.png')
+        self.image = pygame.transform.scale(self.image, (20 * N, 30 * N))
+        self.image = pygame.transform.flip(self.image, True, False)
+        self.rect = pygame.rect.Rect(1460 * N + screen.get_width() // 2, 650 * N + screen.get_height() // 2, 20 * N, 30 * N)
 
     def update(self):
-        if pygame.sprite.collide_mask(self, main_character):
-            print(respawn_cords[0], global_cords[0])
-            print(respawn_cords[1], global_cords[1])
-            main_character.rect.x = 22800 - respawn_cords[0] - global_cords[0] - main_character.rect.x + screen.get_width()
-            if not respawn_cords[0]:
-                main_character.rect.x -= screen.get_width() // 2
-            main_character.rect.y = 23800 - respawn_cords[1] - global_cords[1] - main_character.rect.y + screen.get_height()
+        if pygame.sprite.spritecollideany(self, character):
+            pass
+            #end_of_game()
 
-            # background_image.image = pygame.transform.scale(load_image('astral_word_background.png'),
-            #                                                 (screen.get_width() * 2, screen.get_height()))
+
+
